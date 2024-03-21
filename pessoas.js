02f52d07654084e2a0bf036b5bf31bb84afdffec
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-21 15:04:29 -0300

    Passo 3B

--- pessoas.js
+++ pessoas.js
@@ -11,12 +11,17 @@ let pessoa1 = new Pessoa("Fax",21,"Divinopolis");
 let pessoa2 = new Pessoa("Rhei",33,"Londrina");
 let pessoa3 = new Pessoa("Miguel",23,"Londrina");
 
+let pessoaNova = new Pessoa("Sura",29,"Londrina");
+
 const listaDePessoas = [pessoa1,pessoa2,pessoa3];
+
+listaDePessoas.push(pessoaNova);
 
 const mostrarListaPessoas = (listaDePessoas) => {
     const dados = listaDePessoas.forEach(pessoa => {
         console.log(pessoa.nome + "\n" + pessoa.idade +"\n"+ pessoa.cidade + "\n-----------------");
     });
 }
+
 
 mostrarListaPessoas(listaDePessoas);
