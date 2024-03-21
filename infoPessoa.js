1e0ca80f50f386f7a8f8aaf6576cf149bbb0a9c1
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-21 14:45:39 -0300

    Passo 2

--- infoPessoa.js
+++ infoPessoa.js
@@ -4,6 +4,11 @@ const pessoa = {
     idade: 21,
     solteiro: true,
     hobbies: ["lol","academia","ouvir música"],
+    endereco: {
+        rua:"Alameda José Pinheiro",
+        cidade: "Capivara do Leste",
+        estado: "Mato Grosso do Sul",
+    }
 }
 
 const mostrarInfoPessoa = (pessoa) => {
@@ -11,6 +16,7 @@ const mostrarInfoPessoa = (pessoa) => {
     console.log(typeof pessoa.idade +" "+ pessoa.idade);
     console.log(typeof pessoa.solteiro +" "+ pessoa.solteiro);
     console.log(typeof pessoa.hobbies +" "+ pessoa.hobbies);
+    console.log(typeof pessoa.endereco +" "+ pessoa.endereco.rua+ "\n"+pessoa.endereco.cidade+"\n"+pessoa.endereco.estado);
 }
 
 mostrarInfoPessoa(pessoa);
